386657dbd36246c8f1984b0c202324f71d92dcf4
Alex Kim <alexkim5682@gmail.com>
2024-05-12 22:44:10 +1000

    Finished coin change

---
+++ 322.coin-change.py
@@ -0,0 +1,45 @@
+#
+# @lc app=leetcode id=322 lang=python3
+#
+# [322] Coin Change
+#
+
+# @lc code=start
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        
+        # Memo Solution
+
+        # memo = {}
+        # def findMin(amount):
+        #     if amount < 0: return float('inf')
+        #     if amount == 0: return 0
+        #     if amount in memo: return memo[amount]
+            
+        #     res = float('inf')
+        #     for coin in coins:
+        #         res = min(res, findMin(amount - coin) + 1)
+        
+        #     memo[amount] = res
+
+        #     return res
+
+        # res = findMin(amount)
+        # return res if res != float('inf') else - 1
+            
+
+        # Tabulation     
+
+        dp = [float('inf')] * (amount + 1)
+        dp[0] = 0
+
+        for i in range(1, amount + 1):
+            for coin in coins:
+                if i >= coin:
+                    dp[i] = min(dp[i], dp[i - coin] + 1)
+        
+        return dp[amount] if dp[amount] != float('inf') else -1
+
+        
+# @lc code=end
+
